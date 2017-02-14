87d2149723c3c9fde7eff51ea69c3707687a1621
Sarah Betack <msbetack@gmail.com>
2017-02-14 12:19:41 -0600

    Complete release 2

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,10 +3,12 @@ puts "What is the hamster's name?"
 hamster_name = gets.chomp
 puts "How loud is the hamster on a scale from 1 to 10."
 volume_level = gets.chomp
+volume_level = volume_level.to_i
 puts "What is the fur color?"
 fur_color = gets.chomp
 puts "Is the hamster adoptable (yes/no)?"
 is_adoptable = gets.chomp
 puts "What is the hamster's age?"
 age = gets.chomp
+age = age.to_i
 
