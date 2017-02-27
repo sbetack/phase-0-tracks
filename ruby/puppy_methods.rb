4d35c9731ceb728ca70ae4d13fc3f15b647428e3
Sarah Betack <msbetack@gmail.com>
2017-02-26 18:49:06 -0600

    Finish Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -6,7 +6,7 @@ class Puppy
   end
 
   def speak(int)
-    int.times {print "Woof! "}
+    int.times {puts "Woof!"}
   end
 
   def roll_over
@@ -16,6 +16,10 @@ class Puppy
   def dog_years(human_age)
     dog_age = human_age * 7
     puts dog_age
+  end
+
+  def human_mode
+    puts "*stands on hind legs*"
   end
 
 end
@@ -31,3 +35,5 @@ Roo.speak(5)
 Roo.roll_over
 
 Roo.dog_years(2)
+
+Roo.human_mode
