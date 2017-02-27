04a0ffcc1bec5e8c8f950a20690dcd216187aa06
Sarah Betack <msbetack@gmail.com>
2017-02-26 18:45:10 -0600

    Create instance methods speak, roll_over, and dog_years

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times {print "Woof! "}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_age)
+    dog_age = human_age * 7
+    puts dog_age
+  end
+
 end
 
+  
+
+Roo = Puppy.new
+
+Roo.fetch("ball")
+
+Roo.speak(5)
+
+Roo.roll_over
+
+Roo.dog_years(2)
