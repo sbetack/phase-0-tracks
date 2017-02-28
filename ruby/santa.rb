8c7635c50d787c6a69b474cdb4ee8c17e9cbeb84
Sarah Betack <msbetack@gmail.com>
2017-02-28 12:31:42 -0600

    Refactor code: release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,13 @@
 class Santa
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
   def initialize(gender, ethnicity)
-   puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @age = 0
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
-
-  
-
+  #####INSTANCE METHODS#####
   def celebrate_birthday
     age += 1
   end
@@ -38,6 +37,7 @@ class Santa
   def gender=(new_gender)
     @gender = new_gender
   end
+######################################################################################
 
 #####DRIVER CODE######
 
@@ -54,17 +54,31 @@ class Santa
     genders =['female', 'male', 'bigender', 'gender fluid', 'n/a']
     ethnicities = ['white', 'black', 'latino', 'asian', 'american indian', 'eskimo']
 
-  # santas = []
-  # 20.times do
-  #   random_i_genders = rand(0..4)
-  #   random_i_ethinicites = rand(0..5)
-  #   new_santa = Santa.new(genders[random_i_genders], ethnicities[random_i_ethinicites])
-  #   santas << new_santa
-  # end
-  # p santas
+######################################################################################
+
+#loop to populate Santa class
+  santas = []
+  20.times do
+    random_i_genders = rand(0..4)
+    random_i_ethinicites = rand(0..5)
+    new_santa = Santa.new(genders[random_i_genders], ethnicities[random_i_ethinicites])
+    santas << new_santa
+  end
+  p santas
+
+######################################################################################
+
 end
 
-# test_santa = Santa.new("female", "white")
-# p test_santa.ethnicity
-# test_santa.gender=("male")
-# p test_santa
+#####DRIVER CODE TO TEST SETTER/GETTER METHODS######
+test_santa = Santa.new("female", "white")
+p test_santa
+test_santa.gender=("male")
+p test_santa
+######################################################################################
+
+
+
+
+
+
