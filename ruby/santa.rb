2b3f22eee8490f94551de62ab3993b604cf925f3
Sarah Betack <msbetack@gmail.com>
2017-02-28 14:05:51 -0600

    Complete 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
   attr_reader :age, :ethnicity
-  attr_accessor :gender
+  attr_accessor :gender, :age
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
@@ -47,8 +47,10 @@ class Santa
   pirate_santa.speak
   pirate_santa.eat_milk_and_cookies("chocolate chip")
   pirate_santa.age
+  p "********************************************************************************"
   p pirate_santa
   pirate_santa.get_mad_at("Dancer")
+  p "********************************************************************************"
   p pirate_santa
 
     genders =['female', 'male', 'bigender', 'gender fluid', 'transgender']
@@ -57,14 +59,13 @@ class Santa
 ######################################################################################
 
 #loop to populate Santa class
-  santas = []
-  1000.times do
+  1000.times do |i|
     random_gender = genders.sample
     random_ethinicity = ethnicities.sample
     new_santa = Santa.new(random_gender, random_ethinicity)
-    santas << new_santa
+    new_santa.age= rand(1..140)
+    puts "Santa #{i+1}: #{new_santa.gender}, #{new_santa.ethnicity}, #{new_santa.age}"
   end
-  p santas.last
 
 ######################################################################################
 
@@ -73,9 +74,8 @@ end
 #####DRIVER CODE TO TEST SETTER/GETTER METHODS######
 p "**********************************************************************************"
 test_santa = Santa.new("female", "white")
-
 p test_santa
-test_santa.gender=("male")
+test_santa.gender = "male"
 p "**********************************************************************************"
 p test_santa
 p "**********************************************************************************"
