352247e6e2a7199f6d9edfcf471b8e626ddb79be
Sarah Betack <msbetack@gmail.com>
2017-03-01 09:27:46 -0600

    complete 6.4 release

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,43 @@
-module Shout
-  def self.yell_angrily(words)
+########STANDALONE VERSION##########
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words.upcase + "!!!" + " :)"
+#   end
+  
+#   p yell_angrily("are you fucking kidding me")
+#   p yelling_happily("wahoo, this is awesome")
+
+# end
+
+#########MIXIN VERSION############
+module Shout 
+
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words.upcase + "!!!" + " :)"
   end
-  
-  p yell_angrily("shit")
-  p yelling_happily("wahoo, this is awesome")
-  
+
 end
 
+class Taxi_driver
+  include Shout
+end
+
+class Italian
+  include Shout
+end
+
+
+taxi_driver = Taxi_driver.new
+italian = Italian.new
+
+p taxi_driver.yell_angrily("Get out of the fucking way")
+
+p italian.yelling_happily("hello, how are you")
