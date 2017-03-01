98bbaae137d9e970a8fcf45e376e2a5b92dfbb8a
Sarah Betack <msbetack@gmail.com>
2017-03-01 11:12:51 -0600

    Create todo_list.rb and make methods for test and pass get_item test

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,18 @@
+class TodoList
+	def initialize(items_array)
+		@list_items = items_array
+	end
+
+	def get_items
+		@list_items
+	end
+
+	def add_item(item)
+	end
+
+	def delete_item(item)
+	end
+
+	def get_item(index)
+	end
+end
