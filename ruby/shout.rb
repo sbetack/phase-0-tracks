b63a603341d39236781c2c38ba685a3fde0ee863
Sarah Betack <msbetack@gmail.com>
2017-03-01 09:09:20 -0600

    Create new method in shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,14 @@
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words.upcase + "!!!" + " :)"
+  end
+  
+  p yell_angrily("shit")
+  p yelling_happily("wahoo, this is awesome")
+  
 end
+
