4e32b73dc9864612217936a4428e04d3a1b0c24e
Sarah Betack <msbetack@gmail.com>
2017-03-02 16:52:29 -0600

    Game allows player to continously guess letter in the phrase forever

--- ruby/game/guess_the_word.rb
+++ ruby/game/guess_the_word.rb
@@ -1,12 +1,12 @@
 class GuessTheWord
-	attr_accessor :phrase, :letter_guess_count, :is_over
+	attr_accessor :phrase, :letter_guess_count, :is_over, :all_letter_guesses
 	
 	def initialize(phrase='')
 		@phrase = phrase
 		@phrase_with_letter_guesses = ''
+		@all_letter_guesses = []
 		@letter_guess_count = 0
 		@is_over = false
-		
 	end
 
 	def calc_max_letter_guesses
@@ -22,17 +22,27 @@ class GuessTheWord
 		false
 	end
 
-	def returns_phrase_with_letter_guess(letter_guess)
+	def letter_guess_is_new(letter_guess)
+		if !@all_letter_guesses.include?(letter_guess)
+			@all_letter_guesses << letter_guess
+			return true
+		else
+			return false
+		end
+	end
+
+	def returns_phrase_with_letter_guess
+		@phrase_with_letter_guesses = ''
 		@phrase.split('').each do |letter|
-			if letter == letter_guess
-				@phrase_with_letter_guesses += letter_guess
+			if @all_letter_guesses.include?(letter)
+				@phrase_with_letter_guesses += letter
 			elsif letter != ' '
-				@phrase_with_letter_guesses += '_ '
+				@phrase_with_letter_guesses += '_'
 			else 
-				@phrase_with_letter_guesses += '  '
+				@phrase_with_letter_guesses += ' '
 			end
-		end	
-		@phrase_with_letter_guesses 
+		end
+		p @phrase_with_letter_guesses 
 	end
 
 	def user_2_wins(user_2_guess)
@@ -43,34 +53,43 @@ end
 
 
 #############user-interface############
-# puts "Hey welcome to the two-player game GUESS THE WORD!! Pick a player-one and a player-two"
-# new_game = GuessTheWord.new()
+puts "Hey welcome to the two-player game GUESS THE WORD!! Pick a player-one and a player-two"
+new_game = GuessTheWord.new()
 
-# puts "Shh!!! Player-one, enter you're secret word or phrase."
-# new_game.phrase = gets.chomp
-# new_game.calc_max_letter_guesses
-# while !new_game.is_over
-# 	puts "Hey Player-two! Guess a letter that is included in Player-one's secret phrase"
-# 	letter_guess = gets.chomp
-# 	new_game.letter_guess_count += 1
-# 	if new_game.phrase_contains_letter_guess(letter_guess)
-# 		new_game.returns_phrase_with_letter_guess(letter_guess)
-# 		puts "Would you like to guess the word? (yes or no)"
-# 			wants_to_guess_word = gets.chomp
-# 		if wants_to_guess_word == 'yes'
-# 			puts "What is your word guess?"
-# 			word_guess = gets.chomp
-# 			if new_game.user_2_wins(word_guess)
-# 				puts "Congrats, you got it right! The word is #{new_game.phrase}"
-# 			else
-# 				puts "Nope, sorry the word was #{new_game.phrase}"
-# 			end
-# 			new_game.is_over = true
-# 		end
-# 	else
-# 		puts "Sorry, there is no #{letter_guess} in this phrase."
-# 	end
-# end
+puts "Shh!!! Player-one, enter you're secret word or phrase."
+new_game.phrase = gets.chomp
+new_game.calc_max_letter_guesses
+while !new_game.is_over
+	puts "Hey Player-two! Guess a letter that is included in Player-one's secret phrase"
+	letter_guess = gets.chomp
+	new_game.letter_guess_count += 1
+	if new_game.phrase_contains_letter_guess(letter_guess)
+		if new_game.letter_guess_is_new(letter_guess)
+			new_game.returns_phrase_with_letter_guess
+			puts "Would you like to guess the word? (yes or no)"
+			wants_to_guess_word = gets.chomp
+			if wants_to_guess_word == 'yes'
+				puts "What is your word guess?"
+				word_guess = gets.chomp
+				if new_game.user_2_wins(word_guess)
+					puts "Congrats, you got it right! The word is #{new_game.phrase}"
+				else
+					puts "Nope, sorry the word was #{new_game.phrase}"
+				end
+				new_game.is_over = true
+			end
+		else
+			puts "Oops, you already guessed #{letter_guess}"
+		end
+	else
+		puts "Sorry, there is no #{letter_guess} in this phrase."
+	end
+end
 
+
+
+
+
+##Still need to fix phrase_with_letter_guesses
 
 
