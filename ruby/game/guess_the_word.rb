8907265d8a4ef89ccdef02ae0f0beba0f2c73d17
Sarah Betack <msbetack@gmail.com>
2017-03-02 14:15:41 -0600

    All methods seem to be working expect for the return phrase with letter guess method

--- ruby/game/guess_the_word.rb
+++ ruby/game/guess_the_word.rb
@@ -1,6 +1,6 @@
 class GuessTheWord
-	attr_reader :letter_guess_count, :is_over
-	attr_accessor :phrase
+	attr_reader :is_over
+	attr_accessor :phrase, :letter_guess_count
 	
 	def initialize
 		@phrase = ''
@@ -49,9 +49,29 @@ new_game = GuessTheWord.new
 
 puts "Shh!!! Player-one, enter you're secret word or phrase."
 new_game.phrase = gets.chomp
-
+new_game.calc_max_letter_guesses
 while !new_game.is_over
 	puts "Hey Player-two! Guess a letter that is included in Player-one's secret phrase"
 	letter_guess = gets.chomp
+	new_game.letter_guess_count += 1
+	if new_game.phrase_contains_letter_guess(letter_guess)
+		new_game.returns_phrase_with_letter_guess(letter_guess)
+		puts "Would you like to guess the word? (yes or no)"
+			wants_to_guess_word = gets.chomp
+		if wants_to_guess_word == 'yes'
+			puts "What is your word guess?"
+			word_guess = gets.chomp
+			if user_2_wins(word_guess)
+				puts "Congrats, you got it right! The word is #{new_game.phrase}"
+			else
+				puts "Nope, sorry the word was #{new_game.phrase}"
+			end
+			new_game.is_over = true
+		end
+	else
+		puts "Sorry, there is no #{letter_guess} in this phrase."
+	end
+end
+
 
 
