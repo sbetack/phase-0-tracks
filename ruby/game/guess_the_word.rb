81e8f22802295e5cc181e9ed6d88a7d3da966acb
Sarah Betack <msbetack@gmail.com>
2017-03-02 15:36:42 -0600

    All greengit add -A

--- ruby/game/guess_the_word.rb
+++ ruby/game/guess_the_word.rb
@@ -1,9 +1,9 @@
 class GuessTheWord
-	attr_reader :is_over
-	attr_accessor :phrase, :letter_guess_count
+	attr_accessor :phrase, :letter_guess_count, :is_over
 	
-	def initialize
-		@phrase = ''
+	def initialize(phrase='')
+		@phrase = phrase
+		@phrase_with_letter_guesses = ''
 		@letter_guess_count = 0
 		@is_over = false
 		
@@ -23,17 +23,16 @@ class GuessTheWord
 	end
 
 	def returns_phrase_with_letter_guess(letter_guess)
-		phrase_with_letter_guesses = ''
 		@phrase.split('').each do |letter|
 			if letter == letter_guess
-				phrase_with_letter_guesses += letter_guess
+				@phrase_with_letter_guesses += letter_guess
 			elsif letter != ' '
-				phrase_with_letter_guesses += '_ '
+				@phrase_with_letter_guesses += '_ '
 			else 
-				phrase_with_letter_guesses += '  '
+				@phrase_with_letter_guesses += '  '
 			end
 		end	
-		p phrase_with_letter_guesses 
+		@phrase_with_letter_guesses 
 	end
 
 	def user_2_wins(user_2_guess)
@@ -44,34 +43,34 @@ end
 
 
 #############user-interface############
-puts "Hey welcome to the two-player game GUESS THE WORD!! Pick a player-one and a player-two"
-new_game = GuessTheWord.new
+# puts "Hey welcome to the two-player game GUESS THE WORD!! Pick a player-one and a player-two"
+# new_game = GuessTheWord.new()
 
-puts "Shh!!! Player-one, enter you're secret word or phrase."
-new_game.phrase = gets.chomp
-new_game.calc_max_letter_guesses
-while !new_game.is_over
-	puts "Hey Player-two! Guess a letter that is included in Player-one's secret phrase"
-	letter_guess = gets.chomp
-	new_game.letter_guess_count += 1
-	if new_game.phrase_contains_letter_guess(letter_guess)
-		new_game.returns_phrase_with_letter_guess(letter_guess)
-		puts "Would you like to guess the word? (yes or no)"
-			wants_to_guess_word = gets.chomp
-		if wants_to_guess_word == 'yes'
-			puts "What is your word guess?"
-			word_guess = gets.chomp
-			if user_2_wins(word_guess)
-				puts "Congrats, you got it right! The word is #{new_game.phrase}"
-			else
-				puts "Nope, sorry the word was #{new_game.phrase}"
-			end
-			new_game.is_over = true
-		end
-	else
-		puts "Sorry, there is no #{letter_guess} in this phrase."
-	end
-end
+# puts "Shh!!! Player-one, enter you're secret word or phrase."
+# new_game.phrase = gets.chomp
+# new_game.calc_max_letter_guesses
+# while !new_game.is_over
+# 	puts "Hey Player-two! Guess a letter that is included in Player-one's secret phrase"
+# 	letter_guess = gets.chomp
+# 	new_game.letter_guess_count += 1
+# 	if new_game.phrase_contains_letter_guess(letter_guess)
+# 		new_game.returns_phrase_with_letter_guess(letter_guess)
+# 		puts "Would you like to guess the word? (yes or no)"
+# 			wants_to_guess_word = gets.chomp
+# 		if wants_to_guess_word == 'yes'
+# 			puts "What is your word guess?"
+# 			word_guess = gets.chomp
+# 			if new_game.user_2_wins(word_guess)
+# 				puts "Congrats, you got it right! The word is #{new_game.phrase}"
+# 			else
+# 				puts "Nope, sorry the word was #{new_game.phrase}"
+# 			end
+# 			new_game.is_over = true
+# 		end
+# 	else
+# 		puts "Sorry, there is no #{letter_guess} in this phrase."
+# 	end
+# end
 
 
 
--- ruby/game/guess_the_word_spec.rb
+++ ruby/game/guess_the_word_spec.rb
@@ -1,29 +1,29 @@
 require_relative 'guess_the_word'
 
 describe GuessTheWord do 
-	let(:game) { GuessTheWord.new }
-	game.phrase = "hello"
+	let(:game) { GuessTheWord.new('hello') }
+
 	it "Calculates the max letter guess count" do
-		expect(game.calc_max_letter_guesses("hello")).to eq 2
+		expect(game.calc_max_letter_guesses).to eq 2
 	end 
 
 	it "Checks phrase for letter guess when true" do
-		expect(game.phrase_contains_letter_guess("hello", "o")).to eq true 
+		expect(game.phrase_contains_letter_guess("o")).to eq true 
 	end
 
 	it "Checks phrase for letter guess when false" do
-		expect(game.phrase_contains_letter_guess("hello", "z")).to eq false
+		expect(game.phrase_contains_letter_guess("z")).to eq false
 	end
 
 	it "Returns the phrase with the correct letter guesses" do 
-		expect(game.returns_phrase_with_letter_guess("hello", "l")).to eq "_ _ ll_ "
+		expect(game.returns_phrase_with_letter_guess("l")).to eq "_ _ ll_ "
 	end
 
 	it "Returns true if the word guess is correct" do
-		expect(game.user_2_wins("hello", "hello")).to eq true
+		expect(game.user_2_wins("hello")).to eq true
 	end
 
 	it "Returns false if the word guess is incorrect" do
-		expect(game.user_2_wins("hello", "yello")).to eq false
+		expect(game.user_2_wins("yello")).to eq false
 	end
 end
