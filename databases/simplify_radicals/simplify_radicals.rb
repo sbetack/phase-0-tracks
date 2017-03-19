5e70e919683323a4f974bcee06a069369bb94f25
Sarah Betack <msbetack@gmail.com>
2017-03-19 14:33:41 -0500

    simplifies coefficient of radicand and simplifies variables of radicand

--- databases/simplify_radicals/simplify_radicals.rb
+++ databases/simplify_radicals/simplify_radicals.rb
@@ -80,6 +80,8 @@ def is_perfect_square?(number)
   Math.sqrt(number) == Math.sqrt(number).to_i
 end
 
+#returns a hash the key is the number that moves outside of the radical and the value is the number that stay inside the radical
+#for example, sqrt(20) would result in {2 => 5} because the simplified form is 2sqrt(5)
 def simplify_coefficient_of_radicand(radical_expression)
   radicand = get_the_radicand(radical_expression)
   radicand_coeff = get_the_coefficient(radicand)
@@ -96,17 +98,58 @@ def simplify_coefficient_of_radicand(radical_expression)
   end
   simplified_coefficient
 end
-#returns a hash the key is the number that moves outside of the radical and the value is the number that stay inside the radical
-#for example, sqrt(20) would result in {2 => 5} because the simplified form is 2sqrt(5)
+
+
+
+
+#this is a hash that maps each variable letter to it's corresponding exponent for ex: x^6yz^3 returns {'x' => 6, 'y' => 1, 'z' => 3}
+def separate_variables_with_corresponding_exponents(variable_expression)
+  variables_mapped_to_exponents = {}
+  variable_expression.chars.each.with_index do |character, index|
+    if (is_letter(character)) && (index == variable_expression.length - 1)
+      variables_mapped_to_exponents[character] = 1 
+    elsif is_letter(character) && (is_letter(variable_expression[index+1]))
+      variables_mapped_to_exponents[character] = 1 
+    elsif is_letter(character) && (variable_expression[index+1] == '^')
+      variables_mapped_to_exponents[character] = variable_expression[index+2].to_i
+    end
+  end
+  variables_mapped_to_exponents 
+end
+
+#this returns a nested array where the zero index of each of the arrays inside is what will go outside of the radical and the first index is what will go inside the radical
+# for ex: xy^3z^6 returns [[1, "x"], ["y^1", "y"], ["z^3", 1]]
+# this needs to be a nested array because hashes do not allow for repeated keys 
+def simplify_variables_in_radicand(radical_expression)
+  radicand = get_the_radicand(radical_expression)
+  if get_the_variable(radicand) != ''
+    radicand_vars = get_the_variable(radicand)
+    vars_mapped_to_expons = separate_variables_with_corresponding_exponents(radicand_vars)
+    p vars_mapped_to_expons
+    simplified_vars = []
+    vars_mapped_to_expons.each do |var, expon|
+      if expon.even?
+        expon_outside_rad = expon/2
+        simplified_vars.push(["#{var}^#{expon_outside_rad}", 1])
+      elsif expon.odd? && expon != 1
+        expon_outside_rad = (expon-1)/2
+        simplified_vars.push(["#{var}^#{expon_outside_rad}","#{var}"])
+      else
+        simplified_vars.push([1, "#{var}"])
+      end    
+    end
+  end
+  simplified_vars
+end
 
 
 
 p radicand = get_the_radicand("8xy^2sqrt(20xy^2z)")
 p get_the_coefficient(radicand)
 p get_the_variable(radicand)
-
+p separate_variables_with_corresponding_exponents("xy^2z^6")
 p simplify_coefficient_of_radicand("8xy^2sqrt(468xy^2z)")
-
+p simplify_variables_in_radicand("8xy^2sqrt(468xy^3z^6)")
 
 
 
