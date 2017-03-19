0d9d721841035e0d7cb3b881b6416fa798abf2ff
Sarah Betack <msbetack@gmail.com>
2017-03-19 17:21:12 -0500

    create method to populate database, but did not it so database is empty

--- databases/simplify_radicals/simplify_radicals.rb
+++ databases/simplify_radicals/simplify_radicals.rb
@@ -37,6 +37,7 @@
 
 # __________________________________________________________
 
+########################makes the database table
 # require 'sqlite3'
 # db = SQLite3::Database.new("simplify_rads.db") 
 
@@ -50,7 +51,15 @@
 # SQL
 
 # db.execute(create_table_cmd)
+# # __________________________________________________________
+# #########populating database
 
+# list_of_problems = ['sqrt(64x^2y)', 'sqrt(40a^3b^7c)', 'sqrt(100)', 'sqrt(42a^2c^18)', 'sqrt(6408p^18q^123r^70)', 'sqrt(25xyz)', 'sqrt(68ab^7c)', 'sqrt(225x^8y)', 'sqrt(20x^7y^8)', 'sqrt(30f^7g^10)', 'sqrt(500s^7t^19)']
+# list_of_problems.each do |radical_expression|
+#   db.execute("INSERT INTO simplifying_radical_problems (problem, solution, level_of_difficulty) VALUES (?, ?, ?)", [radical_expression, simplify_radical_expression(radical_expression), rate_difficulty(radical_expression)])
+# end
+# __________________________________________________________
+ 
 #takes in the radical expressions as strings using sqrt() to expression radicals
 def has_add_sub_or_div(radical_expression)
   has_add = radical_expression.include?"+"
